ba657b9e692bd908a3bfa85810fb4cf382b102df
musukeki1000 <musukeki1000@gmail.com>
2025-07-09 17:19:04 +0800

    add front list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './front/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'front/list', component: ListComponent }
+];
