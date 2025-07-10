f2bf51c5ac2b1e3993026c54a5bf738bd9d9f88a
musukeki <musukeki1000@gmail.com>
2025-07-11 00:39:45 +0800

    add ng material & set front/list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { LoginComponent } from './login/login.component';
 import { AnswerComponent } from './front/answer/answer.component';
 import { CheckComponent } from './front/check/check.component';
 import { StatisticalComponent } from './front/statistical/statistical.component';
+import { BackListComponent } from './back/back-list/back-list.component';
 
 export const routes: Routes = [
   { path: 'front/list', component: ListComponent },
   { path: 'front/answer', component: AnswerComponent },
   { path: 'login', component: LoginComponent },
   { path: 'front/check', component: CheckComponent },
-  { path: 'front/statistical', component: StatisticalComponent }
+  { path: 'front/statistical', component: StatisticalComponent },
+
+  { path: 'back/backList', component: BackListComponent }
 ];
---
+++ src/app/back/back-list/back-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-list',
+  imports: [],
+  templateUrl: './back-list.component.html',
+  styleUrl: './back-list.component.scss'
+})
+export class BackListComponent {
+
+}
