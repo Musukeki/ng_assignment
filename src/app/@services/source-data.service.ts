30347c65e799554b518c741bc8d7e3ae1808f672
musukeki <musukeki1000@gmail.com>
2025-07-16 21:29:08 +0800

    還原點

---
+++ src/app/@services/source-data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SourceDataService {
+
+  // sourceData: Array<any> = [
+  //   { number: 1, name: '最佳員工', status: '尚未開始', startDate: '2025/07/18', endDate: '2025/07/24', result: false },
+  //   { number: 2, name: '台灣超市比拼調查', status: '進行中', startDate: '2025/07/12', endDate: '2025/07/18', result: true },
+  //   { number: 3, name: '最美員工', status: '已結束', startDate: '2025/07/09', endDate: '2025/07/15', result: true },
+  //   { number: 4, name: '最醜員工', status: '尚未發布', startDate: '2025/07/20', endDate: '2025/07/26', result: true },
+  //   { number: 5, name: '最胖員工', status: '進行中', startDate: '2025/07/13', endDate: '2025/07/19', result: true },
+  //   { number: 6, name: '最有錢員工', status: '進行中', startDate: '2025/07/13', endDate: '2025/07/19', result: true },
+  //   { number: 7, name: '最窮員工', status: '已結束', startDate: '2025/07/10', endDate: '2025/07/16', result: true }
+  // ];
+
+  constructor() { }
+}
