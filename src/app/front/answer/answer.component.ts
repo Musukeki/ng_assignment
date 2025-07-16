6c1d77b0dad3fb4931b455508951a0095da56d18
musukeki <musukeki1000@gmail.com>
2025-07-16 15:10:39 +0800

    update

--- src/app/front/answer/answer.component.ts
+++ src/app/front/answer/answer.component.ts
@@ -1,11 +1,12 @@
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { UsersService } from './../../@services/users.service';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 @Component({
   selector: 'app-answer',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './answer.component.html',
   styleUrl: './answer.component.scss'
 })
@@ -23,6 +24,49 @@ export class AnswerComponent {
   userAnswer1!: string;
 
   userAnswer2!: string;
+
+  // 假資料
+
+  quesData = {
+    id: 1,
+    title: '台灣超市比拼調查',
+    startDate: '2025/07/12',
+    endDate: '2025/07/18',
+    description: '台灣的四大超商指的是：7-11 (統一超商), 全家便利商店, 萊爾富, 和OK 超商。 這些超商在台灣的零售市場中佔據主要地位，提供各種商品和服務，包括日常用品、食品、代收代付、以及包裹取貨等。這些超商不僅是提供日常必需品的場所，也逐漸轉型成為生活服務中心，提供多元化的服務，如代收費用、包裹寄送、影印、提款等，成為台灣民眾生活中不可或缺的一部分。',
+    questArr: [
+      {
+        questId: 1,
+        isRequired: true,
+        questName: '您最常光顧的超市',
+        type: 'single',
+        questList: [
+          { name: '7-11', code: 'A' },
+          { name: '全家', code: 'B' },
+          { name: '萊爾富', code: 'C' },
+          { name: 'OK', code: 'D' }
+        ]
+      },
+      {
+        questId: 2,
+        isRequired: true,
+        questName: '平均一週光顧次數',
+        type: 'multiple',
+        questList: [
+          { name: '3 次以下', code: 'A' },
+          { name: '3 ~ 7 次', code: 'B' },
+          { name: '7 次以上', code: 'C' }
+        ]
+      },
+      {
+        questId: 3,
+        isRequired: true,
+        questName: '請簡述這家超市和其他的差異',
+        type: 'text',
+        textContent: '我不知道'
+      },
+    ]
+  }
+
 
   ngOnInit(): void {
     // this.usersService.userNameService = this.userNameAnswer;
--- src/app/front/list/list.component.ts
+++ src/app/front/list/list.component.ts
@@ -57,7 +57,7 @@ export interface PeriodicElement {
 
 const ELEMENT_DATA: PeriodicElement[] = [
   { number: 1, name: '最佳員工', status: '尚未開始', startDate: '2025/07/18', endDate: '2025/07/24', result: false },
-  { number: 2, name: '最差員工', status: '進行中', startDate: '2025/07/12', endDate: '2025/07/18', result: true },
+  { number: 2, name: '台灣超市比拼調查', status: '進行中', startDate: '2025/07/12', endDate: '2025/07/18', result: true },
   { number: 3, name: '最美員工', status: '已結束', startDate: '2025/07/09', endDate: '2025/07/15', result: true },
   { number: 4, name: '最醜員工', status: '尚未發布', startDate: '2025/07/20', endDate: '2025/07/26', result: true },
   { number: 5, name: '最胖員工', status: '進行中', startDate: '2025/07/13', endDate: '2025/07/19', result: true },
@@ -65,7 +65,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { number: 7, name: '最窮員工', status: '已結束', startDate: '2025/07/10', endDate: '2025/07/16', result: true }
 ];
 
-// mat-paginator 底部文字設定
+// mat-paginator
 export function getCustomPaginatorIntl(): MatPaginatorIntl {
   const paginatorIntl = new MatPaginatorIntl();
 
