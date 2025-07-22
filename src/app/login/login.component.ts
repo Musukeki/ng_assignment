08728d6ced0639e2fdd5994858ccf2a082c1301b
musukeki <musukeki1000@gmail.com>
2025-07-22 22:11:34 +0800

    新增登入帳密假資料

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  standalone: true, // 這是關鍵
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
+  testAccount!: string;
+  testPassword!: string;
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.testAccount === '001' && this.testPassword === '001') {
+      this.router.navigate(['/front/list']);
+    } else if (this.testAccount === '000' && this.testPassword === '000') {
+      this.router.navigate(['/back/backList']);
+    } else {
+      alert('F:001/001&B:000/000')
+    }
+  }
 }
